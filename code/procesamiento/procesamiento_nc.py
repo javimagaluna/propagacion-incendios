173340d7d75fe407d29f4ff7e2b51fc37d9e85c0
Javiera <javiera.preuss@uc.cl>
2025-06-23 01:30:15 -0400

    angulo y calidad de bandas en nc

--- code/procesamiento/procesamiento_nc.py
+++ code/procesamiento/procesamiento_nc.py
@@ -3,6 +3,7 @@ import re
 import os
 
 import rioxarray
+import xarray as xr
 import geopandas as gpd
 import pandas as pd
 
@@ -22,8 +23,8 @@ areas = gpd.read_file(path_areas)
 ###  identificar pares .nc
 regex = re.compile(r"A\d{7}\.\d{4}\.\d{3}")  ## expresion regular para mapear los archivos
 
-path_bandas = Path(r'datos-viirs\NOAA2\BANDAS')
-path_coords = Path(r'datos-viirs\NOAA2\COORDS') 
+path_bandas = Path(r'datos-viirs\SUOMI\BANDAS')
+path_coords = Path(r'datos-viirs\SUOMI\COORDS') 
 
 archivos_bandas = {regex.search(f.name).group(): f for f in path_bandas.glob("*.nc") if regex.search(f.name)}
 archivos_coords = {regex.search(f.name).group(): f for f in path_coords.glob("*.nc") if regex.search(f.name)}
@@ -77,7 +78,7 @@ def filtrar_por_areas_y_unir(gdf, areas):
 ## --------------------------------
 
 
-path_save = 'data/procesado/satellite_data/noaa2'
+path_save = 'data/procesado/satellite_data/suomi'
 
 for bandas, coordenadas in pares:
     nombre_archivo = regex.search(str(bandas)).group()
@@ -90,27 +91,55 @@ for bandas, coordenadas in pares:
             
     coords = rioxarray.open_rasterio(coordenadas)
     data_nasa = rioxarray.open_rasterio(bandas)
+    data_nasa.data_vars
+    data_nasa.attrs
     
-    lat_lon = coords[0][['latitude', 'longitude']].isel(band=0).to_dataframe().reset_index()
-    data_bandas = data_nasa.isel(band=0).to_dataframe().reset_index()
+    # VNP03IMG coordenadas y angulos
+    lat = coords[0]['latitude'].isel(band=0)
+    lon = coords[0]['longitude'].isel(band=0)
+    sensor_zenith = (coords[0]['sensor_zenith'].isel(band=0) * coords[0].sensor_zenith.attrs['scale_factor'] )+ coords[0].sensor_zenith.attrs['add_offset']
+    sensor_azimuth = (coords[0]['sensor_azimuth'].isel(band=0) * coords[0].sensor_azimuth.attrs['scale_factor'] )+ coords[0].sensor_azimuth.attrs['add_offset']
     
-    ## aplicar factor de escala
-    data_bandas['I04_scale'] = (data_bandas['I04'] * data_nasa.I04.attrs['scale_factor'] )+ data_nasa.I04.attrs['add_offset']
-    data_bandas['I05_scale'] = (data_bandas['I05'] * data_nasa.I05.attrs['scale_factor'] )+ data_nasa.I05.attrs['add_offset']
-
-    print('merge data')
-    data_lat_lon = lat_lon.merge(data_bandas, on = ['y', 'x', 'band', 'spatial_ref'], how = 'inner')
+    # VNP02IMG bandas termicas escaladas
+    i04_scaled = (data_nasa['I04'].isel(band=0) * data_nasa.I04.attrs['scale_factor'] )+ data_nasa.I04.attrs['add_offset']
+    i05_scaled = (data_nasa['I05'].isel(band=0) * data_nasa.I05.attrs['scale_factor'] )+ data_nasa.I05.attrs['add_offset']
     
-    data_lat_lon = data_lat_lon.drop(columns = ['band', 'spatial_ref', 'I04_uncert_index', 'I05_uncert_index'],
-                                 axis= 1)
+    ## incertidumbre -> nos sirve para evaluar la precision del valor
+    I04_uncert_index = 1 + data_nasa.I04_uncert_index.attrs['scale_factor'] * (data_nasa['I04_uncert_index'].isel(band=0) ** 2)
+    I05_uncert_index = 1 + data_nasa.I05_uncert_index.attrs['scale_factor'] * (data_nasa['I05_uncert_index'].isel(band=0) ** 2)
+    
+    print('creando df')
+    
+    ds = xr.Dataset({
+        'latitude': lat,
+        'longitude': lon,
+        'sensor_zenith': sensor_zenith,
+        'sensor_azimuth': sensor_azimuth,
+        'I04': i04_scaled,
+        'I05': i05_scaled,  
+        'I04_quality_flags':data_nasa['I04_quality_flags'].isel(band=0), 
+        'I05_quality_flags': data_nasa['I05_quality_flags'].isel(band=0), 
+        'quality_flag': coords[0]['quality_flag'].isel(band=0),
+        'I04_uncert_index': I04_uncert_index,
+        'I05_uncert_index': I05_uncert_index
+    })
+    
+    df = ds.to_dataframe().reset_index()
+    df = df.dropna(subset=['latitude', 'longitude', 'I04', 'I05']) 
+    df = df.drop(['x','y', 'band', 'spatial_ref'], axis= 1)
     
     print('to geopandas')
-    gdf = gpd.GeoDataFrame(data_lat_lon, geometry=gpd.points_from_xy(data_lat_lon.longitude,
-                                                                     data_lat_lon.latitude), crs="EPSG:4326")
+    gdf = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df.longitude,
+                                                           df.latitude), crs="EPSG:4326")
     
     gdf_unido = filtrar_por_areas_y_unir(gdf, areas)
-    print(gdf_unido.zona.value_counts()) 
     del gdf
+    
+    if gdf_unido.shape[0]==0:
+        print('no hay datos dentro de las zonas')
+        continue
+    
+    print(gdf_unido.zona.value_counts()) 
     
     print('save file')
     gdf_unido.to_file(f'{path_save}/merge_{nombre_archivo}.geojson')
---
+++ code/viz.py
@@ -0,0 +1,146 @@
+import matplotlib.pyplot as plt
+from matplotlib.colors import Normalize
+
+import os
+from pathlib import Path
+
+import geopandas as gpd
+import pandas as pd
+import numpy as np
+import re
+
+from datetime import datetime, timedelta
+
+from shapely.geometry import Polygon, Point, box
+from shapely.affinity import rotate, translate
+import pyproj
+
+import imageio
+
+#########################################################
+def extraer_fecha(cadena):
+    año = int(cadena[1:5])
+    dia_juliano = int(cadena[5:8])
+    hora = int(cadena[9:11])
+    minuto = int(cadena[11:13])
+    
+    fecha = datetime(año, 1, 1) + timedelta(days=dia_juliano - 1)
+    fecha = fecha.replace(hour=hora, minute=minuto)
+    return fecha
+
+def make_pixel_square(point, size=375):
+    half = size / 2
+    return box(point.x - half, point.y - half, point.x + half, point.y + half)
+
+
+def make_viirs_pixel(lat, lon, zenith_deg, azimuth_deg, size_nadir=375):
+    """Crea pixel deformado segun angulo de observacion"""
+    zenith_rad = np.deg2rad(zenith_deg)
+    width = size_nadir / np.cos(zenith_rad)
+    height = size_nadir
+
+    # cuadrado centrado en (0,0)
+    hw, hh = width / 2, height / 2
+    rect = Polygon([(-hw, -hh), (-hw, hh), (hw, hh), (hw, -hh)])
+
+    # rotar por azimuth
+    pixel_rot = rotate(rect, -azimuth_deg, origin=(0, 0), use_radians=False)
+
+    # proyectar punto a metros
+    proj = pyproj.Transformer.from_crs("EPSG:4326", "EPSG:5070", always_xy=True)
+    x0, y0 = proj.transform(lon, lat)
+
+    # trasladar al centroide
+    pixel_proj = translate(pixel_rot, xoff=x0, yoff=y0)
+
+    # a lat/lon
+    back_proj = pyproj.Transformer.from_crs("EPSG:5070", "EPSG:4326", always_xy=True)
+    return Polygon([back_proj.transform(x, y) for x, y in pixel_proj.exterior.coords])
+
+
+
+#########################################################
+path_areas = Path('data/procesado/zonas_incendios/areas_buffer.geojson')
+areas = gpd.read_file(path_areas)
+satelite = 'suomi'
+path_save = 'data/procesado/satellite_data/union'
+
+carpeta = f"data/procesado/satellite_data/{satelite}"
+archivos = [f.name for f in Path(carpeta).iterdir() if f.is_file()]
+regex = re.compile(r"A\d{7}\.\d{4}\.\d{3}") 
+
+
+## load data suomi
+gdf_zonas = []
+for id in range(len(archivos)): 
+    
+    arch = archivos[id]
+    print(arch)
+    fecha = regex.search(str(arch)).group()
+    data = gpd.read_file(carpeta + '/' + arch)
+    data['date_time'] = extraer_fecha(fecha)
+    data['date_file'] = fecha
+    
+    gdf_zonas.append(data)
+
+gdf_zonas = gpd.GeoDataFrame(pd.concat(gdf_zonas, ignore_index=True), crs=gdf_zonas[0].crs) 
+gdf_zonas.to_file(f'{path_save}/zonas_{satelite}.geojson')
+
+
+### filtrando zona
+aux = gdf_zonas[gdf_zonas.eval('zona == "area2"')]
+aux.date_time.value_counts()
+
+### filtrando por calidad
+aux = aux[aux['I04_quality_flags']== 0]
+aux.date_time.value_counts()
+
+### creando buffer
+gdf_m = aux.to_crs("EPSG:5070") 
+gdf_m['geometry'] = gdf_m.apply(lambda row: make_viirs_pixel(row['latitude'],
+                                                             row['longitude'],
+                                                             row['sensor_zenith'],
+                                                             row['sensor_azimuth']
+                                                             ),
+                                axis=1)
+
+gdf_pixel_polys = gpd.GeoDataFrame(gdf_m, geometry='geometry', crs='EPSG:4326')
+
+## load healpix
+
+grilla = gpd.read_file('data/procesado/grilla/areas_grilla_healpix.geojson')
+
+
+# Crear carpeta temporal
+os.makedirs("frames", exist_ok=True)
+
+# Escalar los valores una sola vez
+vmin = gdf_pixel_polys['I04'].min()
+vmax = gdf_pixel_polys['I04'].max()
+norm = Normalize(vmin=vmin, vmax=vmax)
+
+grilla_filt = grilla[grilla.eval('zona == "area2"')]
+
+tiempo = gdf_pixel_polys.date_time.unique()
+for fecha in tiempo:
+    print(fecha)
+    gdf =  gdf_pixel_polys[gdf_pixel_polys['date_time'] == fecha]
+    
+    fig, ax = plt.subplots(figsize=(8, 6))
+    grilla_filt.plot(ax=ax, facecolor='none', edgecolor='gray', linewidth=0.5)
+    gdf.plot(ax=ax, column="I04", cmap='viridis', edgecolor='black', alpha=0.4, norm=norm)
+    
+    ax.set_title(f"Incendios - {str(gdf['date_time'].iloc[0])}")
+    ax.set_aspect("equal")
+    ax.axis('off')
+    
+    # Colorbar
+    sm = plt.cm.ScalarMappable(cmap='viridis', norm=norm)
+    sm._A = []
+    cbar = fig.colorbar(sm, ax=ax, orientation='horizontal', fraction=0.046, pad=0.04)
+    cbar.set_label("I04")
+    
+    # Guardar frame
+    frame_path = f"frames/frame_{gdf.date_file.iloc[0]}.png"
+    plt.savefig(frame_path, bbox_inches='tight', dpi=150)
+    plt.close()
